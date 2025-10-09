acf1121ad97561a21548c67592fb806ff23151a8
Kristina Pivoriunaite <pivoriunaite.kristina@gmail.com>
2025-10-09 15:12:42 +0200

    String empty space remover with test

--- src/main/java/StringManipulations/RemoveSpaces.java
+++ src/main/java/StringManipulations/RemoveSpaces.java
@@ -2,4 +2,9 @@ package StringManipulations;
 //Remove all whitespace characters from a string
 //Example: "hello world" → "helloworld"
 public class RemoveSpaces {
+
+    public String removeSpace(String input) {
+        if(input == null) return "";
+        return input.replace(" ", "");
+    }
 }
