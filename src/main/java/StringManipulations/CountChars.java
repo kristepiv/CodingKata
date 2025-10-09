8549abe65ba89e816dffbb33d043022159dad01a
Kristina Pivoriunaite <pivoriunaite.kristina@gmail.com>
2025-10-09 14:58:39 +0200

    Vowels and Consonants counter with test

--- src/main/java/StringManipulations/CountChars.java
+++ src/main/java/StringManipulations/CountChars.java
@@ -1,5 +1,33 @@
 package StringManipulations;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 //Count the number of vowels and consonants in a string
 //Example: "hello" → vowels: 2, consonants: 3
 public class CountChars {
+
+    static final Set<Character> VOWEL_SET = Set.of('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+
+    static boolean isVowelUsingSet(char c) {
+        return VOWEL_SET.contains(c);
+    }
+
+    static Map<String, Integer> countVowelsAndConsonants(String input) {
+        if (input == null) return null;
+        input = input.replace(" ", "");
+        char[] chars = input.toCharArray();
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Vowels", 0);
+        map.put("Consonants", 0);
+        for (char aChar : chars) {
+            if (isVowelUsingSet(aChar)) {
+                map.put("Vowels", map.get("Vowels") + 1);
+            } else if (Character.isAlphabetic(aChar)) {
+                map.put("Consonants", map.get("Consonants") + 1);
+            }
+        }
+        return map;
+    }
 }
---
+++ src/test/java/StringManipulations/CountCharsTest.java
@@ -0,0 +1,57 @@
+package StringManipulations;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static StringManipulations.CountChars.countVowelsAndConsonants;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountCharsTest {
+
+    @Test
+    public void countCharsTestForNullInput() {
+        assertNull(countVowelsAndConsonants(null));
+    }
+
+    @Test
+    public void countCharsTestForEmptyInput() {
+        Map<String, Integer> expectedResult = new HashMap<>();
+        expectedResult.put("Vowels", 0);
+        expectedResult.put("Consonants", 0);
+        assertEquals(expectedResult, CountChars.countVowelsAndConsonants(""));
+    }
+
+    @Test
+    public void countCharsTest() {
+        Map<String, Integer> expectedResult = new HashMap<>();
+        expectedResult.put("Vowels", 2);
+        expectedResult.put("Consonants", 3);
+        assertEquals(expectedResult, CountChars.countVowelsAndConsonants("hello"));
+    }
+
+    @Test
+    public void countCharsTestWithEmptySpaces() {
+        Map<String, Integer> expectedResult = new HashMap<>();
+        expectedResult.put("Vowels", 2);
+        expectedResult.put("Consonants", 3);
+        assertEquals(expectedResult, CountChars.countVowelsAndConsonants(" h e l lo"));
+    }
+
+    @Test
+    public void countCharsTestWithNumbers() {
+        Map<String, Integer> expectedResult = new HashMap<>();
+        expectedResult.put("Vowels", 2);
+        expectedResult.put("Consonants", 3);
+        assertEquals(expectedResult, CountChars.countVowelsAndConsonants("12300hello"));
+    }
+
+    @Test
+    public void countCharsTestOnlyWithSymbols() {
+        Map<String, Integer> expectedResult = new HashMap<>();
+        expectedResult.put("Vowels", 0);
+        expectedResult.put("Consonants", 0);
+        assertEquals(expectedResult, CountChars.countVowelsAndConsonants("12300@#$%?"));
+    }
+}
